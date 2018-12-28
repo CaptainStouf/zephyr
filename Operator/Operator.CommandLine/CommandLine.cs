7b8f91f8b158b5be0797d02d3c2922d30bedd0c4
Stéphane Guerreau <stouf@hardware-libre.org>
2018-12-28 14:49:24 +0100

    Added local message from operator
    Send a local message to Zephyr from a local operator. Forked from https://github.com/12downsk/zephyr

---
+++ Operator/Operator.CommandLine/CommandLine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operator;
+namespace Operator.CommandLine
+{
+    public class CommandLine
+    {
+        public static void Start()
+        {
+            
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            bool firstLoop = true;
+
+            while (true)
+            {
+                
+                string messageText = String.Empty;
+                Console.ForegroundColor = ConsoleColor.White;
+                string title = "Operator";
+                Console.Clear();
+                if (true)
+                {
+                    Console.Write("Send an empty message to exit.\r\n");
+                    firstLoop = false;
+                }
+                Console.Write("message >");
+                messageText = Console.ReadLine();
+                //Console.WriteLine($">[{DateTime.Now.ToShortTimeString()}] - {messageText}");
+                if (String.IsNullOrWhiteSpace(messageText))
+                    return;
+                var message = new Message(new Payload(title, messageText));
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                MessageSender.SendMessage(message);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+    }
+}
---
+++ Operator/Operator/Payload.cs
@@ -0,0 +1,23 @@
+﻿namespace Operator
+{
+    public class Payload
+    {
+        public Payload() { }
+
+        public Payload(string title, string text)
+        {
+            this.id = 0;
+            this.device = "zephyr-desktop-client-OperatorPlugin";
+            this.title = title;
+            this.text = text;
+        }
+
+        public int id;
+
+        public string title;
+
+        public string text;
+
+        public string device;
+    }
+}
